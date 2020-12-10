1cce838b79a57775a089c05cb86da0ab6c43d430
NicholasMurray <murray.nicholas@gmail.com>
2020-12-10 12:14:51 +0000

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import { FaStar } from "react-icons/fa";
+
+const createArray = (length) => [
+  ...Array(length)
+];
+
+function Star({selected = false, onSelect }) {
+  return <FaStar 
+    color={selected ? "red" : "gray" } 
+    onClick={onSelect}
+  />;
+}
+
+function StarRating({totalStars = 5}) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n,i) => (
+        <Star 
+          key={i} 
+          selected={selectedStars > i} 
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>{selectedStars} of total stars</p>
+    </>
+  );
+}
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <StarRating totalStars={5} />
   );
 }
