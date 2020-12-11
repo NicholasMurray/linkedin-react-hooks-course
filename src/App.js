8f219133f20f4bb0ade56f17fd617677cea5d264
NicholasMurray <murray.nicholas@gmail.com>
2020-12-11 18:06:24 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { useReducer } from "react";
 import "./style.css";
 
+const initialState = {
+  message: "hi"
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "yell":
+      return {
+        message: `HEY! I JUST SAID ${state.message}`
+      };
+    case "whisper":
+      return {
+        message: `excuse me, I just said ${state.message}`
+      };
+  }
+}
+
 export default function App() {
-  const [checked, toggle] = useReducer((checked) => !checked, false);
+
+  const [state, dispatch] = useReducer(
+    reducer,
+    initialState
+  )
+
   return (
     <>
-    <input
-      type="checkbox"
-      value={checked}
-      onChange={toggle}
-      />
-      {checked ? "checked" : "not checked"}
-  </>
+      <p>Message: {state.message}</p>
+      <button onClick={() => dispatch({type: "yell"})}>Yell</button>
+      <button onClick={() => dispatch({type: "whisper"})}>whisper</button>
+    </>
   );
 }
