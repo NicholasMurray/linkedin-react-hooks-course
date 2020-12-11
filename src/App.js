210907e7087a46749a314b987c7e91737baa3119
NicholasMurray <murray.nicholas@gmail.com>
2020-12-11 16:04:34 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import React, { useState, useEffect } from "react";
 import "./style.css";
 
 export default function App() {
-  const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     fetch(`https://api.github.com/users`)
       .then(response => response.json())
-      .then(setData);
+      .then(setUsers);
   }, []);
 
-  if (data) {
+  if (users) {
+    console.log(users);
     return (
       <div>
         <p>Github Users</p>
         <ul>
-          {data.map(user => {
-            <li key={user.id}>{user.login}</li>;
-          })}
+          {users.map(user => (
+            <li key={user.id}>{user.login}</li>
+          ))}
         </ul>
-        <button onClick={() => setData([])}>Remove Data</button>
+        <button onClick={() => setUsers([])}>Remove Data</button>
       </div>
     );
   }
