e04aa8d9c7b8a9136f331f04719836e19474c224
NicholasMurray <murray.nicholas@gmail.com>
2020-12-11 17:52:10 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer } from "react";
 import "./style.css";
 
 export default function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users`)
-      .then(response => response.json())
-      .then(setUsers);
-  }, []);
-
-  if (users) {
-    console.log(users);
-    return (
-      <div>
-        <p>Github Users</p>
-        <ul>
-          {users.map(user => (
-            <li key={user.id}>{user.login}</li>
-          ))}
-        </ul>
-        <button onClick={() => setUsers([])}>Remove Data</button>
-      </div>
-    );
-  }
-  return <p>No users</p>;
+  const [checked, toggle] = useReducer((checked) => !checked, false);
+  return (
+    <>
+    <input
+      type="checkbox"
+      value={checked}
+      onChange={toggle}
+      />
+      {checked ? "checked" : "not checked"}
+  </>
+  );
 }
